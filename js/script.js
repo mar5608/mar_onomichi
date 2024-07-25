5e71d09a05b4231bef44a9d2be9d618c67bdb975
mar <137989121+mar5608@users.noreply.github.com>
2024-07-26 00:32:11 +0900

    footerの一部までSP版

--- js/script.js
+++ js/script.js
@@ -22,21 +22,21 @@ jQuery("#js-drawer-icon").on("click", function (e) {
 //   }
 // });
 
-const swiper = new Swiper(".swiper", {
+const swiperAbout = new Swiper("#js-about-swiper", {
   // Optional parameters
   // direction: "vertical",
   loop: true, //無限再生
   slidesPerView: 3, // 一度に表示する枚数
-  // spaceBetween: 10, //横のカードとの間
+  // spaceBetween: 16, //横のカードとの間
   breakpoints: {
     // スライドの表示枚数：768px以上の場合
-    768: {
-      slidesPerView: 4,
-    },
-    // // スライドの表示枚数：1024px以上の場合
-    1024: {
-      slidesPerView: 5,
-    },
+    // 768: {
+    //   slidesPerView: 4,
+    // },
+    // // // スライドの表示枚数：1024px以上の場合
+    // 1024: {
+    //   slidesPerView: 5,
+    // },
     // スライドの表示枚数：1440px以上の場合
     1440: {
       slidesPerView: 6,
@@ -47,4 +47,46 @@ const swiper = new Swiper(".swiper", {
   autoplay: {
     delay: 0, // 途切れなくループ
   },
+});
+
+const swiperSpot = new Swiper("#js-spot-swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true, //無限再生
+  slidesPerView: 1, // 一度に表示する枚数
+  spaceBetween: 10, //横のカードとの間
+  breakpoints: {
+    // スライドの表示枚数：768px以上の場合
+    768: {
+      slidesPerView: 1,
+    },
+    // // スライドの表示枚数：1024px以上の場合
+    1024: {
+      slidesPerView: 2,
+    },
+    // スライドの表示枚数：1440px以上の場合
+    1440: {
+      slidesPerView: 3,
+    },
+  },
+  speed: 6000, // ループの時間
+  allowTouchMove: false, // スワイプ無効
+  autoplay: {
+    delay: 0, // 途切れなくループ
+  },
+});
+
+/** qa js-accordion */
+jQuery(".js-accordion").on("click", function (e) {
+  e.preventDefault();
+
+  if (jQuery(this).parent().hasClass("is-open")) {
+    // thisで押した要素を取得する
+    jQuery(this).parent().removeClass("is-open");
+    // スライドしながら戻る
+    jQuery(this).next().slideUp();
+  } else {
+    jQuery(this).parent().addClass("is-open");
+    jQuery(this).next().slideDown();
+  }
 });
