eb79ffba4c2309ae700f1e2e02280228e9ee860b
mar <137989121+mar5608@users.noreply.github.com>
2024-10-19 23:39:24 +0900

    headerのアニメーション追加、ホバー時のリンクの動きも追加

--- js/script.js
+++ js/script.js
@@ -9,6 +9,35 @@ jQuery("#js-drawer-icon").on("click", function (e) {
   jQuery("#js-drawer-content").toggleClass("is-checked");
 });
 
+/** スムーススクロール*/
+/** drawer 制御 */
+jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
+
+jQuery('a[href^="#"]').on("click", function (e) {
+  const speed = 300;
+  const id = jQuery(this).attr("href");
+  const target = jQuery("#" == id ? "html" : id);
+  const position = jQuery(target).offset().top;
+  jQuery("html,body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing" // swing or liner
+  );
+  // jQuery("html,body").animate(
+  //   {
+  //     scrollTop: 0,
+  //   },
+  //   1000,
+  //   "swing" // swing or liner
+  // );
+});
+
 /* ****************************************
 ABOUT
 swiper
@@ -102,5 +131,14 @@ jQuery(".js-accordion").on("click", function (e) {
   } else {
     jQuery(this).parent().addClass("is-open");
     jQuery(this).next().slideDown();
+  }
+});
+
+/** topに戻る */
+jQuery(window).on("scroll", function () {
+  if (100 < jQuery(window).scrollTop()) {
+    jQuery("#js-pagetop").addClass("is-show");
+  } else {
+    jQuery("#js-pagetop").removeClass("is-show");
   }
 });
